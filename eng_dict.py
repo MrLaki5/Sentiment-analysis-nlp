c0e3ea2fbe1b175bb6584b0349240845629f81ee
predragoetf <predragoetf@gmail.com>
2019-06-04 23:23:12 +0200

    Add function for building eng dictionary

---
+++ eng_dict.py
@@ -0,0 +1,20 @@
+pathPositive = "lexicons/English/translations/positive-prevedeno-sredjeno.txt"
+pathNegative = "lexicons/English/translations/negative-prevedeno-sredjeno.txt"
+
+def constructEngLexicon(positives, negatives):
+    engDict = {}
+    with open(positives, encoding='utf-8') as f1:
+        for line in f1:
+            word = line[:line.find("\n")]
+            engDict[word] = 1
+    with open(negatives, encoding='utf-8') as f2:
+        for line in f2:
+            word = line[:line.find("\n")]
+            if word in engDict:    
+                engDict[word] = 0
+            else:
+                engDict[word] = -1
+    return engDict      
+            
+engDict = constructEngLexicon(pathPositive, pathNegative)
+    
