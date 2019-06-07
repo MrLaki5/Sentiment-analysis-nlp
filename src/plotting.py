a1d11cba6783d1ab66e4fd7eb6fcf6d42104a68a
Pedja <predragoetf@gmail.com>
2019-06-07 16:40:15 +0200

    Add normalized confusion matrix calculation to plotting.py

--- src/plotting.py
+++ src/plotting.py
@@ -1,5 +1,6 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.metrics import confusion_matrix
 
 LABELS_TWO_CLASS = ['Negative', 'Positive']
@@ -17,11 +18,24 @@ def calculate_confusion_matrix(y_true, y_pred, class_labels):
     ax.yaxis.set_ticklabels(class_labels)
     return cm
 
+def calculate_normalized_confusion_matrix(y_true, y_pred, class_labels):
+    cm = confusion_matrix(y_true, y_pred)
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    ax = plt.subplot()
+    sns.heatmap(cm, annot=True, ax=ax, cmap='Greens')
+    ax.set_xlabel('Predicted labels')
+    ax.set_ylabel('True labels')
+    ax.set_title('Confusion Matrix')
+    ax.xaxis.set_ticklabels(class_labels)
+    ax.yaxis.set_ticklabels(class_labels)
+    return cm
+
 def show_confusion_matrix():
     plt.show()
 
-
-#calculate_confusion_matrix(y_true, y_pred, LABELS_THREE_CLASS)
+#y_true = [1, 1, 2, 2, 1, 0]
+#y_pred = [1, 1, 2, 1, 2, 0]
+#calculate_normalized_confusion_matrix(y_true, y_pred, LABELS_THREE_CLASS)
 #show_confusion_matrix()
 #calculate_confusion_matrix(y_true2, y_pred2, LABELS_TWO_CLASS)
 #show_confusion_matrix()
