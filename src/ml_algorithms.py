5f0370a1ab33cc64f36a2724f56c495fffe0b16d
Emanuilo <emmi_book@hotmail.com>
2019-06-09 14:09:53 +0200

    svm, log reg. funkcija za broj pojavljivanja reci u komentaru

--- src/ml_algorithms.py
+++ src/ml_algorithms.py
@@ -1,9 +1,12 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
 import numpy as np
@@ -12,15 +15,34 @@ import tokenizer
 import plotting
 from sklearn.pipeline import Pipeline
 
+def getCountVector(class_num=2):
+    if class_num == 2:
+        train_data = pd.read_csv('../movie_dataset/SerbMR-2C.csv')
+    else:
+        train_data = pd.read_csv('../movie_dataset/SerbMR-3C.csv')
+    count_vect = CountVectorizer(tokenizer=tokenizer.text_to_tokens)
+    train_X_counts = count_vect.fit_transform(train_data['Text'])
+    return count_vect
+
+def getOccurNumberDictionary(text, count_vect):
+    dict_counter = {}
+    for index, word in enumerate(count_vect.get_feature_names()):
+        dict_counter[word] = 0
+
+    for word in tokenizer.text_to_tokens(text):
+        if word in dict_counter:
+            dict_counter[word] += 1
+    return dict_counter
+
+
 def training(algorithm, class_num=2):
     if class_num == 2:
         train_data = pd.read_csv('../movie_dataset/SerbMR-2C.csv')
     else:
         train_data = pd.read_csv('../movie_dataset/SerbMR-3C.csv')
-    #train_X, val_X, train_y, val_y = train_test_split(train_data['Text'], train_data['class-att'], test_size=0.2)
 
-    splits = 5
-    kf = KFold(n_splits=splits, shuffle=True, random_state=1)
+    splits = 2
+    kf = KFold(n_splits=splits, shuffle=True)
     accuracy = 0
 
     # KFold(n_splits=2, random_state=None, shuffle=False)
@@ -30,42 +52,29 @@ def training(algorithm, class_num=2):
         val_X = [train_data['Text'][i] for i in test_index]
         val_y = [train_data['class-att'][i] for i in test_index]
 
-        # count_vect = CountVectorizer(tokenizer=tokenizer.text_to_tokens)
-        # train_X_counts = count_vect.fit_transform(train_X)
-        # val_X_counts = count_vect.transform(val_X)
-        #
-        # tfidf_transformer = TfidfTransformer()
-        # train_X_tfidf = tfidf_transformer.fit_transform(train_X_counts)
-        # val_X_tfidf = tfidf_transformer.transform(val_X_counts)
-        #
-        # algorithm.fit(train_X_tfidf, train_y)
-        #
-        # y_pred = algorithm.predict(val_X_tfidf)
-
-        ####
-        parameters = {
-            'vect__ngram_range': [(1, 1), (1, 2)],
-            'tfidf__use_idf': (True, False),
-            'clf__alpha': (1e-2, 1e-3)
-        }
+        # parameters = {
+        #     'vect__ngram_range': [(1, 1), (1, 2)],
+        #     'tfidf__use_idf': (True, False),
+        #     'clf__alpha': (1e-2, 1e-3)
+        # }
 
         text_clf = Pipeline([
             ('vect', CountVectorizer(tokenizer=tokenizer.text_to_tokens)),
             ('tfidf', TfidfTransformer()),
+            # ('tfidf', TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), tokenizer=tokenizer.text_to_tokens)),
             ('clf', algorithm),
         ])
 
-        gs_clf = GridSearchCV(text_clf, parameters, cv=5, n_jobs=-1)
+        # gs_clf = GridSearchCV(text_clf, parameters, cv=5, n_jobs=-1)
 
         text_clf.fit(train_X, train_y)
         y_pred = text_clf.predict(val_X)
-        ######
 
-        # if class_num == 2:
-        #     plotting.calculate_normalized_confusion_matrix(val_y, y_pred, 2)
-        # else:
-        #     plotting.calculate_normalized_confusion_matrix(val_y, y_pred, 3)
-        # plotting.show_confusion_matrix()
+        if class_num == 2:
+            plotting.calculate_normalized_confusion_matrix(val_y, y_pred, 2)
+        else:
+            plotting.calculate_normalized_confusion_matrix(val_y, y_pred, 3)
+        plotting.show_confusion_matrix()
 
         # print(gs_clf.best_score_)
         # for param_name in sorted(parameters.keys()):
@@ -79,10 +88,19 @@ def SVM(class_num=2):
     svm = SGDClassifier(loss='hinge', penalty='l2',
                         alpha=1e-3, random_state=42,
                         max_iter=5, tol=None)
+    svc = LinearSVC()
     return training(svm, class_num)
 
 def naive_bayes(class_num=2):
     return training(MultinomialNB(), class_num)
 
+def log_reg(class_num=2):
+    return training(LogisticRegression(C=30, dual=True), class_num)
 
-# print(SVM(2))
+def decision_tree(class_num=2):
+    return training(DecisionTreeClassifier(max_depth=100), class_num)
+
+# print(naive_bayes(3))
+# print(SVM(3))
+# print(log_reg(3))
+# print(decision_tree(2))
